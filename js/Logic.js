ef26b251cc65ed52f029f9b4288cf5ebd1d4d76d
maxifernandez1999 <fmaximiliano443@gmail.com>
2020-12-17 13:09:03 -0300

    Cambios

--- js/Logic.js
+++ js/Logic.js
@@ -1,26 +1,15 @@
 
 
 (function(){
-	var mostrarImagen = function(){
-		var elemento = document.getElementById("imagenLavarropas");
-		if (elemento.className == "imagenLavarropas") {
-			elemento.className = "imagenLavarropas ver";
-		}else if(elemento.className == "imagenLavarropas ver"){
-			elemento.className = "imagenLavarropas";
-		}
-		
-
-	};
-	var mostrarDescripcion = function(){
-		var elemento2 = document.getElementById("parrafo");
-		if (elemento2.className == "parrafo") {
-			elemento2.className = "parrafo ver";
-
-		}else if(elemento2.className == "parrafo ver"){
-			elemento2.className = "parrafo";
+	var mostrarLavarropas = function(){
+		var elemento = document.getElementById("claseContenedor");
+		if (elemento.className == "claseContenedor") {
+			elemento.className = "claseContenedor ver";
+		}else if(elemento.className == "claseContenedor ver"){
+			elemento.className = "claseContenedor";
 		}
 	};
+	
 	var boton = document.getElementById("enlace");
-	boton.addEventListener("click",mostrarImagen,false);
-	boton.addEventListener("click",mostrarDescripcion,false);
+	boton.addEventListener("click",mostrarLavarropas,false);
 }())
